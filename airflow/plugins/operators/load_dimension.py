42d3eaf443dac21379bd836c3c630b2920675ebe
Tania <tvasilikioti@babbel.com>
2020-10-04 21:35:04 +0200

    add operator to load dimension tables with optional truncation

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -5,18 +5,36 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    sql = """
+        INSERT INTO {}
+        {};
+        """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 destination_table="",
+                 sql_template="",
+                 truncate=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.destination_table = destination_table
+        self.redshift_conn_id = redshift_conn_id
+        self.truncate = truncate,
+        self.sql_template = sql_template
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.truncate:
+            self.log.info(f"Truncating table {self.table}")
+            redshift.run(f"DELETE FROM {self.table}")
+
+        self.log.info(f"Loading data into dim table {self.table}")
+
+        formatted_sql = LoadFactOperator.sql.format(
+            self.table,
+            self.sql_template
+        )
+        redshift.run(formatted_sql)
