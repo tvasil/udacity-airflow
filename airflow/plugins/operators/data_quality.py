887b944d6c80836577b0cf2aff67dff15a9050ea
Tania <tvasilikioti@babbel.com>
2020-10-05 10:20:42 +0200

    generalize quality check operator with params

--- airflow/plugins/operators/data_quality.py
+++ airflow/plugins/operators/data_quality.py
@@ -9,21 +9,29 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
+                 qry_template="",
+                 threshold=1,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.tables = kwargs["params"]["tables"]
+        self.qry_template = qry_template
+        self.threshold = threshold
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
         self.log.info("Running quality checks")
         for t in tables:
-            result = redshift.get_records(f"SELECT COUNT(*) FROM {t}")
-            if len(result) < 1 or len(result[0]) < 1:
+            qry = self.qry_template.format(t)
+            result = redshift.get_records(qry)
+
+            if len(result) < self.threshold or len(result[0]) < self.threshold:
                 raise ValueError(f"Table {t} has no records")
+
             num_records = result[0][0]
-            if num_records < 1:
+            if num_records < self.threshold:
                 raise ValueError(f"Data quality check failed. {t} contained 0 rows")
+
         self.log.info(f"Quality check on table {t} passed with {num_records} records")
